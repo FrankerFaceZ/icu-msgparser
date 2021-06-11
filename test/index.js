182e4059bf2ac58546df49d4390416741799c67c
SirStendec <sir@stendec.me>
2021-06-11 15:40:26 -0400

    test(✅): add tests related to new desired behaviors

--- test/index.js
+++ test/index.js
@@ -23,21 +23,33 @@ describe('Parser', function() {
 
 		it('takes options', function() {
 			const thing = Parser({
-				OPEN: '<',
-				CLOSE: '>'
+				OPEN: '(',
+				CLOSE: ')'
 			});
-			expect(thing.parse('Hello, <name>!')).to.deep.equal([
+			expect(thing.parse('Hello, (name)!')).to.deep.equal([
 				'Hello, ',
 				{v: 'name'},
 				'!'
 			]);
-		})
+		});
 
+		it('does not allow multi-character options', function() {
+			expect(() => Parser({OPEN: '{{', CLOSE: '}}'})).to.throw();
+		});
+
+		it('does not allow matching options', function() {
+			expect(() => Parser({OPEN: '<'})).to.throw();
+		})
 	});
 
 	describe('parse()', function() {
 		it('accepts strings', function() {
 			const msg = 'This is a test.';
+			expect(parse(msg)).to.deep.equal([msg]);
+		})
+
+		it('accepts unicode', function() {
+			const msg = '中文';
 			expect(parse(msg)).to.deep.equal([msg]);
 		})
 
@@ -74,7 +86,7 @@ describe('Parser', function() {
 					t: 'plural',
 					o: {
 						'one': ['one test'],
-						'other': [{v: 'test'}, ' test']
+						'other': [{v: 'test', t: 'number'}, ' test']
 					}
 				}
 			])
@@ -88,7 +100,34 @@ describe('Parser', function() {
 					f: 3,
 					o: {
 						'one': ['one test'],
-						'other': [{v: 'test'}, ' test']
+						'other': [{v: 'test', t: 'number'}, ' test']
+					}
+				}
+			])
+		})
+
+		it('parses plural with =', function() {
+			expect(parse('{test, plural, =0{none} other {# test} }')).to.deep.equal([
+				{
+					v: 'test',
+					t: 'plural',
+					o: {
+						'=0': ['none'],
+						'other': [{v: 'test', t: 'number'}, ' test']
+					}
+				}
+			])
+		})
+
+		it('parses plural with negative offset', function() {
+			expect(parse('{test, plural, offset:-3 =-2{thingy} other {# test} }')).to.deep.equal([
+				{
+					v: 'test',
+					t: 'plural',
+					f: -3,
+					o: {
+						'=-2': ['thingy'],
+						'other': [{v: 'test', t: 'number'}, ' test']
 					}
 				}
 			])
@@ -101,7 +140,7 @@ describe('Parser', function() {
 					t: 'selectordinal',
 					o: {
 						'one': ['one test'],
-						'other': [{v: 'test'}, ' test']
+						'other': [{v: 'test', t: 'number'}, ' test']
 					}
 				}
 			])
@@ -154,16 +193,35 @@ describe('Parser', function() {
 			])
 		})
 
+		it('treats unicode chars as whitespace', function() {
+			expect(parse(`{gender, select,
+			\u202Fmale {{He}}
+			\u205Ffemale {{She}}
+			\u2008other{{They}}}`)).to.deep.equal([
+				{
+					v: 'gender',
+					t: 'select',
+					o: {
+						male: [{v: 'He'}],
+						female: [{v: 'She'}],
+						other: [{v: 'They'}]
+					}
+				}
+			])
+		})
+
 		it('throws on empty variable', function() {
 			expect(() => parse('{}')).to.throw(SyntaxError, 'expected placeholder id')
 		})
 
-		it('throws on extra closing brace', function() {
-			expect(() => parse('}')).to.throw(SyntaxError, 'unexpected }')
+		it('ignores extra closing brace', function() {
+			expect(parse('}')).to.deep.equal(['}'])
 		})
 
 		it('throws on unclosed variable', function() {
 			expect(() => parse('{n')).to.throw(SyntaxError, 'expected , or }')
+			expect(() => parse('{n,number')).to.throw(SyntaxError, 'expected , or }')
+			expect(() => parse('{n,number,short')).to.throw(SyntaxError, 'expected }')
 		})
 
 		it('throws on open brace in variable', function() {
@@ -206,6 +264,127 @@ describe('Parser', function() {
 
 		it('throws on missing offset number', function() {
 			expect(() => parse('{n,plural,offset: other{n}')).to.throw(SyntaxError, 'expected number');
+		})
+
+		it('parses tags', function() {
+			expect(parse('<b>hi</b>')).to.deep.equal([
+				{
+					n: 'b',
+					c: ['hi']
+				}
+			]);
+		})
+
+		it('allows a single solitary tag_open', function() {
+			expect(parse('hi <')).to.deep.equal(['hi <'])
+		})
+
+		it('parses self-closing tags', function() {
+			expect(parse('hi <user-tag />!')).to.deep.equal([
+				'hi ',
+				{n: 'user-tag'},
+				'!'
+			])
+		})
+
+		it('parses empty tags', function() {
+			expect(parse('hi <user-tag></user-tag>!')).to.deep.equal([
+				'hi ',
+				{n: 'user-tag'},
+				'!'
+			])
+		})
+
+		it('requires closing tags', function() {
+			expect(() => parse('<b>hi there')).to.throw(SyntaxError, 'expected closing tag');
+		});
+
+		it('rejects weird closing tags', function() {
+			expect(() => parse('<b>hi</b/>')).to.throw(SyntaxError, 'expected >')
+		});
+
+		it('allows tag characters', function() {
+			expect(parse('i <3 you>')).to.deep.equal([
+				'i <3 you>',
+			]);
+		})
+
+		it('allows spaces in tags', function() {
+			expect(parse('<3 >Hi</3>')).to.deep.equal([
+				{
+					n: '3',
+					c: ['Hi']
+				}
+			])
+		})
+
+		it('parses nested tags', function() {
+			expect(parse('<b><i>hi</i></b>')).to.deep.equal([
+				{
+					n: 'b',
+					c: [
+						{
+							n: 'i',
+							c: ['hi']
+						}
+					]
+				}
+			])
+		})
+
+		it('rejects unexpected closing tags', function() {
+			expect(() => parse('hi</b>')).to.throw(SyntaxError, 'unexpected /')
+		})
+
+		it('allows quotes in tag names', function() {
+			expect(parse("<'/b' />")).to.deep.equal([
+				{
+					n: '/b'
+				}
+			])
+		})
+
+		it('rejects invalid nested tags', function() {
+			expect(() => parse('<b><i>hi</b></i>')).to.throw(SyntaxError, 'tag mismatch')
+		})
+
+		it('escapes tags', function() {
+			expect(parse("'<notATag>hello</notATag>'")).to.deep.equal([
+				'<notATag>hello</notATag>'
+			])
+		})
+
+		it('parses stuff inside tags', function() {
+			expect(parse('Our price is <boldThis>{price, number, ::currency/USD precision-integer}</boldThis> with <link>{pct, number, ::percent} discount</link>')).to.deep.equal([
+				'Our price is ',
+				{
+					n: 'boldThis',
+					c: [
+						{
+							v: 'price',
+							t: 'number',
+							f: '::currency/USD precision-integer'
+						}
+					]
+				},
+				' with ',
+				{
+					n: 'link',
+					c: [
+						{
+							v: 'pct',
+							t: 'number',
+							f: '::percent'
+						},
+						' discount'
+					]
+				}
+			])
+		})
+
+		it('works without tags', function() {
+			const thing = Parser({allowTags: false});
+			expect(thing.parse('<b>hi</b>')).to.deep.equal(['<b>hi</b>']);
 		})
 	});
 
